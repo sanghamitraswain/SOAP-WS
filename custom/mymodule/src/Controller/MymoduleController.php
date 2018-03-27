23ea9bb51a57367a2eb9967104667880c568478c
swain <sanghamitra.swain@capgemini.com>
2018-03-27 17:59:21 +0530

    Consume webservice by SOAP request - Drupal 8

---
+++ custom/mymodule/src/Controller/MymoduleController.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mymodule\Controller\MymoduleController.
+ */
+namespace Drupal\mymodule\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use \SoapClient;
+
+//require_once(drupal_get_path('module', 'mymodule') .'/nusoap/lib/nusoap.php');
+module_load_include('php', 'mymodule','nusoap.php');
+
+/**
+ * Controller.
+ */
+class MymoduleController extends ControllerBase {
+
+  /**
+   * Callback function to get the data from webservice
+   */
+  public function getResponse() {
+	  $apiauth =array('UserName'=>'test','Password'=>'test');
+		$service = new SoapClient("http://www.webservicex.com/globalweather.asmx?WSDL", $apiauth);
+		$country = 'India';
+		$result = $service->GetCitiesByCountry(array('CountryName'=>$country));
+		$response = (array)$result;
+		dpm($response);
+		return $response;
+	}
+}
+
+
+
+
+
+
+
+
+
